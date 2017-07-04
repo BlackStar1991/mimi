f2bb9c779c64cb04ed00691b498f223b03a6a391
wert_and-ПК\wert_and <alexandr_works@ukr.net>
2017-07-04 10:41:40 +0300

    working progect

--- app/js/common.js
+++ app/js/common.js
@@ -1,21 +1,25 @@
 $(document).ready(function () {
 
+     console.log("work");
 
 
-    //AJAX forms send
-    //Docs: http://api.jquery.com/jquery.ajax/
-    $("form").submit(function () {
-        $.ajax({
-            type: "GET",
-            url: "mail.php",
-            data: $("form").serialize()
-        }).done(function () {
-            alert("Спасибо за заявку!");
-            setTimeout(function () {
-                $.fancybox.close();
-            }, 1000);
-        });
-        return false;
-    });
+
+
+
+    // //AJAX forms send
+    // //Docs: http://api.jquery.com/jquery.ajax/
+    // $("form").submit(function () {
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "mail.php",
+    //         data: $("form").serialize()
+    //     }).done(function () {
+    //         alert("Спасибо за заявку!");
+    //         setTimeout(function () {
+    //             $.fancybox.close();
+    //         }, 1000);
+    //     });
+    //     return false;
+    // });
 
 });
